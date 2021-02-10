ccece33f5f23c97ffa2e57902e3c9093fb220a9d
GabrielaPiovezan <gabrielapiovezan@live.com>
2021-02-10 20:12:57 -0300

    Moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,11 +1,29 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
 
 	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+		String s = sc.nextLine();
+		char column = s.charAt(0);
+		int row = Integer.parseInt(s.substring(1));
+		return new ChessPosition(column,row);
+		}
+		catch(RuntimeException e){
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
+		}
+		
+		
+	}
 
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
@@ -38,17 +56,15 @@ public class UI {
 	}
 
 	private static void printPiece(ChessPiece piece) {
-    	if (piece == null) {
-            System.out.print("-");
-        }
-        else {
-            if (piece.getColor() == Color.WHITE) {
-                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            }
-            else {
-                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
-            }
-        }
-        System.out.print(" ");
+		if (piece == null) {
+			System.out.print("-");
+		} else {
+			if (piece.getColor() == Color.WHITE) {
+				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+			} else {
+				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+			}
+		}
+		System.out.print(" ");
 	}
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -49,6 +49,19 @@ public class Board {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
 
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)){
+			throw new BoardException("There is already  a piece on position " + position);		
+		}
+		if(piece(position)== null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}
