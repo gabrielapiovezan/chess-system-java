26f3253b5a9c5942550184952af555f2e9b3ad22
GabrielaPiovezan <gabrielapiovezan@live.com>
2021-02-19 20:22:35 -0300

    tratament exception of promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,5 @@
 package chess;
 
-
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -121,7 +119,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if (!type.equals("B") && !type.equals("R") && !type.equals("N") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+		return promoted;
 		}
 		Position pos = promoted.getChessPosition().toPosition();
 		Piece p = board.removePiece(pos);
